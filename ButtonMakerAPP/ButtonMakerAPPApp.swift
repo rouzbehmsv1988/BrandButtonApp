0b0dab5cbc809a9223bc0075a450eea683653ea0
rouzbeh <rouzbehmsv1988@gmail.com>
2024-02-01 10:19:00 +0100

    Initial Commit

---
+++ ButtonMakerAPP/ButtonMakerAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ButtonMakerAPPApp.swift
+//  ButtonMakerAPP
+//
+//  Created by rouzbeh on 01.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ButtonMakerAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
